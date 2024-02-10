91bc00851af60d2386401b558d02b8db0fb3aad3
Arad Margalit <aradmar@gmail.com>
2024-02-09 16:29:15 -0800

    concessions for light moders

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ display: 'block', subsets: ['latin'], weight: '800' });
 
 export default function Header() {
   return (
-    <header className={`${inter.className} p-4 w-full h-full bg-slate-800`}>
+    <header className={`${inter.className} p-4 w-full h-full dark:bg-slate-800 bg-slate-100`}>
       <Link href="/">
         <h1 className="text-xl">SCREEN/TRIM</h1>
       </Link>
